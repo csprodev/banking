9a8bf684d9683b37cae0af9b61075038e5179513
safril22 <safrilauliarahman@yahoo.com>
2016-12-12 14:23:20 +0700

    commit

---
+++ application/views/sidebar_menu.php
@@ -0,0 +1,9 @@
+<ul class="sidebar-menu">
+  <li class="active"><a href="<?php echo base_url(); ?>"><i class="fa fa-dashboard"></i><span>Dashboard</a></li>
+  <li><a href="<?php echo base_url(); ?>registration"><i class="fa fa-book"></i> <span>Registrasi</span></a></li>
+  <li><a href="<?php echo base_url(); ?>browse_data_master"><i class="fa fa-book"></i> <span>Browse Data Master</span></a></li>
+  <li><a href="<?php echo base_url(); ?>transaction"><i class="fa fa-book"></i> <span>Transaksi</span></a></li>
+  <li><a href="<?php echo base_url(); ?>report"><i class="fa fa-book"></i> <span>Laporan</span></a></li>
+  <li><a href="<?php echo base_url(); ?>upload"><i class="fa fa-book"></i> <span>Upload Data</span></a></li>
+  <li><a href="<?php echo base_url(); ?>log_out"><i class="fa fa-book"></i> <span>Keluar Aplikasi</span></a></li>
+</ul>
