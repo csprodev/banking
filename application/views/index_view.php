d0d07126a75a0a346b4846cc5b170e0c886804ae
safril22 <safrilauliarahman@yahoo.com>
2017-01-20 18:10:47 +0700

    merge

--- application/views/index_view.php
+++ application/views/index_view.php
@@ -89,11 +89,7 @@
         </div>
       </div>
       <!-- search form -->
-<<<<<<< HEAD
       <!-- <form action="#" method="get" class="sidebar-form">
-=======
- <!--      <form action="#" method="get" class="sidebar-form">
->>>>>>> cb97acf3d0631f710e29f6b983a80dfa36582200
         <div class="input-group">
           <input type="text" name="q" class="form-control" placeholder="Search...">
               <span class="input-group-btn">
